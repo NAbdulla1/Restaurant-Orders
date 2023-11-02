7fd8537ce765ef016b595153205a1b1729859f50
Md. Abdulla Al Mamun <abdulla.mamun@enosisbd.com>
2023-11-02 16:02:39 +0600

    Remove unnecessary using statements.

--- Restaurant Orders/Infrastructure/ServiceRegistry.cs
+++ Restaurant Orders/Infrastructure/ServiceRegistry.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Restaurant_Orders.Authorizations;
-using Restaurant_Orders.Services;
 using RestaurantOrder.Core.Extensions;
-using RestaurantOrder.Data.Models;
-using RestaurantOrder.Data.Repositories;
 
 namespace Restaurant_Orders.Infrastructure
 {
