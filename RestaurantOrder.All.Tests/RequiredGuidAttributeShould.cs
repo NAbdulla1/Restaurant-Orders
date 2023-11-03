efdbfda3127b3599b391ee8154c49643c16090e6
Md. Abdulla Al Mamun <abdulla.mamun@enosisbd.com>
2023-11-03 17:25:28 +0600

    Add test for Guid attribute validator.

---
+++ RestaurantOrder.All.Tests/RequiredGuidAttributeShould.cs
@@ -0,0 +1,48 @@
+﻿using RestaurantOrder.Core.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrder.All.Tests
+{
+    [TestFixture]
+    public class RequiredGuidAttributeShould
+    {
+        private RequiredGuidAttribute _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new RequiredGuidAttribute();
+        }
+
+        [Test]
+        public void SuccessfullyValidateNonEmptyGuid()
+        {
+            var guid = Guid.NewGuid();
+
+            var validationResult = _sut.IsValid(guid);
+
+            Assert.That(validationResult, Is.True);
+        }
+
+        [TestCase(null)]
+        [TestCase("00000000-0000-0000-0000-000000000000")]
+        public void FailValidationForEmptyGuid(Guid? guid)
+        {
+            var validationResult = _sut.IsValid(guid);
+
+            Assert.That(validationResult, Is.False);
+        }
+
+        [TestCase("asdf-asdf-asdf-asdf")]
+        public void FailValidationForArbitraryStringValue(string invalidGuid)
+        {
+            var validationResult = _sut.IsValid(invalidGuid);
+
+            Assert.That(validationResult, Is.False);
+        }
+    }
+}
